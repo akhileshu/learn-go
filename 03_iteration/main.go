acdd459300abcdea221d2b887ac87d6253fa16f5
akhileshu <umapati4381@gmail.com>
2026-01-15 02:09:36 +0530

    refactor

--- 03_iteration/main.go
+++ 03_iteration/main.go
@@ -7,7 +7,7 @@ func main() {
 }
 const repeatCount = 5
 func Repeat(s string)  string {
-	// Strings in Go are immutable , so we have to create a new string
+	// Strings in Go are immutable , so every concat operation creates a new string
 	// using the strings.Builder to minimize memory copying
 	var result strings.Builder
 	for range repeatCount {
