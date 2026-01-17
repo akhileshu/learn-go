e5fa5e09184d674a48ce9dba18095ac6980ae8c5
akhileshu <umapati4381@gmail.com>
2026-01-18 01:06:35 +0530

    complete 19_generics

--- 18_templating/main.go
+++ 18_templating/main.go
@@ -1,5 +1,80 @@
 package main
 
+import (
+	// "fmt"
+	"embed"
+	readingfiles "go-playground/17_reading_files"
+	"html/template"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+// var (
+// 	// postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+// postTemplates embed.FS
+// )
+
+// goembed comment is important , maintain syntax
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
 func main() {
 	// entry point
+	filepath.WalkDir(".", func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		println("path:", path)
+		return nil
+	})
+
+}
+func Render(w io.Writer, p readingfiles.Post) error {
+	/*
+		_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<p>%s</p>\n", p.Title, p.Description)
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprintf(w, "Tags: <ul>")
+		if err != nil {
+			return err
+		}
+		for _, t := range p.Tags {
+			_, err = fmt.Fprintf(w, "<li>%s</li>", t)
+			if err != nil {
+				return err
+			}
+
+		}
+		_, err = fmt.Fprintf(w, "</ul>")
+		if err != nil {
+			return err
+		}
+		return err
+
+	*/
+
+	// templ, err := template.New("blog").Parse(postTemplate)
+
+	// 		fs.WalkDir(postTemplates, ".", func(path string, d fs.DirEntry, err error) error {
+	// 			println("path below=========")
+	// 	println(path)
+	// 	return nil
+	// })
+
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
 }
--- 18_templating/main_test.go
+++ 18_templating/main_test.go
@@ -1,7 +1,54 @@
 package main
 
-// import "testing"
+import (
+	"bytes"
+	approvals "github.com/approvals/go-approval-tests"
+	readingfiles "go-playground/17_reading_files"
+	"testing"
+)
 
 // func TestExample(t *testing.T) {
 // 	t.Fatal("not implemented")
 // }
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = readingfiles.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	// TestRender.it_converts_a_single_post_into_HTML.approved.txt contains expected html
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+
+	/*
+			t.Run("it converts a single post into HTML", func(t *testing.T) {
+				buf := bytes.Buffer{}
+				err := Render(&buf, aPost)
+
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				got := buf.String()
+				want := `<h1>hello world</h1>
+		<p>This is a description</p>
+		Tags: <ul><li>go</li><li>tdd</li></ul>`
+
+				if got != want {
+					t.Errorf("got '%s' want '%s'", got, want)
+				}
+			})
+	*/
+}
